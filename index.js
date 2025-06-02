af032900fe2d3a6100624203c9f50ba112cb246c
Priyanshu Jaiswal <priyanshuskt7839@gmail.com>
2025-06-02 17:33:45 +0530

    Autotrends cors Added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express();
 const PORT = 8002;
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin:['http://localhost:5173','https://autotrends.ai']
 }))
 app.use(express.json());
 app.use('/deals',carDetailsRoute)
