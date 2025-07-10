fdfa411220ed8eb35f000ff5839ab9145870b6d6
Priyanshu Jaiswal <priyanshuskt7839@gmail.com>
2025-07-11 00:44:08 +0530

    Cors fix1

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ const cors = require('cors')
 const app = express();
 const PORT = 8002;
 app.use(cors({
-    origin:['http://localhost:5173','https://autotrends.ai','http://localhost:5174']
+    origin:['http://localhost:5173','https://autotrends.ai','http://localhost:5174'],
+     credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }))
+
 app.use(express.json());
 app.use('/deals',carDetailsRoute)
 app.use('/user',userAuthRoute);
