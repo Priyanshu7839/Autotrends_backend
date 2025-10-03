1b191b249cc0805a6a6bd8598593bafec4c07b74
Priyanshu Jaiswal <priyanshuskt7839@gmail.com>
2025-10-03 21:29:15 +0530

    Stock Status added

--- Service/StockHeaderNormalizer.js
+++ Service/StockHeaderNormalizer.js
@@ -1,15 +1,14 @@
 const headerMap = {
   "Stock Age": ["Stock Age", "Ageing"],
-  "Model": ["Model", "Model Group"],
-  "Variant":["Variant","Model Description"],
-  "Exterior Color Name":["Exterior Color Name","Color"],
-  "Vin Number":["Vin Number","VIN"],
-  "Stock Status":["Stock Stauts","Vehicle Status"],
-  "Basic Price":["Basic Price","Net Dealer Price"]
+  Model: ["Model", "Model Group"],
+  Variant: ["Variant", "Model Description"],
+  "Exterior Color Name": ["Exterior Color Name", "Color"],
+  "Vin Number": ["Vin Number", "VIN"],
+  "Stock Status": ["Stock Status", "Vehicle Status"],
+  "Basic Price": ["Basic Price", "Net Dealer Price"],
 };
 
 function normalizeStockRecord(record) {
-    
   const normalized = {};
 
   for (const [canonicalKey, aliases] of Object.entries(headerMap)) {
@@ -17,7 +16,7 @@ function normalizeStockRecord(record) {
     // console.log("   Possible aliases:", aliases);
 
     for (const alias of aliases) {
-    //   console.log("      Checking alias:", alias);
+      //   console.log("      Checking alias:", alias);
 
       if (record[alias] !== undefined) {
         // console.log("      ✅ Found match:", alias, "→", record[alias]);
@@ -29,22 +28,22 @@ function normalizeStockRecord(record) {
 
   // Add untouched keys that weren’t mapped
   for (const key of Object.keys(record)) {
-    const isAlias = Object.values(headerMap).some(aliases => aliases.includes(key));
+    const isAlias = Object.values(headerMap).some((aliases) =>
+      aliases.includes(key)
+    );
     if (!isAlias) {
       normalized[key] = record[key];
     }
   }
 
-  
   return normalized;
-
 }
 
 function normalizeStockArray(rows, headerMap) {
-  return rows.map(row => ({
+  return rows.map((row) => ({
     ...row, // keep everything else
-    stock_data: normalizeStockRecord(row.stock_data, headerMap) // only normalize stock_data
+    stock_data: normalizeStockRecord(row.stock_data, headerMap), // only normalize stock_data
   }));
 }
 
-module.exports = {normalizeStockArray}
+module.exports = { normalizeStockArray };
