2a45b2fc0cde82900559b2576723fe0c6681ddb1
Priyanshu Jaiswal <priyanshujaiswal@MacBookAir.lan>
2026-04-17 04:41:14 +0530

    poolstock download

--- controller/ExcelDownload.controller.js
+++ controller/ExcelDownload.controller.js
@@ -62,8 +62,139 @@ return res.send(buffer);
 
 }
 
+async function DownloadMatchedExcel(req, res) {
+
+    const {dealer_id,selectedDealerCode} = req.params;
+
+
+  const result = await pool.query(` WITH poolstk AS (
+          SELECT 
+              *,
+              (
+                  TRIM(LOWER(COALESCE("Variant Description", ''))) || 
+                  TRIM(LOWER(COALESCE("Ext Color", '')))
+              ) AS d
+          FROM poolstock_data where asm_id = $1
+      ),
+
+      vna_calc AS (
+          SELECT 
+              vna.*,
+              (
+                  TRIM(LOWER(COALESCE(vna."Trim", ''))) || 
+                  TRIM(LOWER(COALESCE(vna."Colour", '')))
+              ) AS e
+          FROM vna where dealer_id = $2 and ($3::TEXT = 'ALL' OR "Code" = $3)
+      )
+
+      SELECT 
+          vna_calc.*,
+
+          COALESCE((
+              SELECT SUM(ps."All India")
+              FROM poolstk ps
+              WHERE ps.d = vna_calc.e
+          ), 0) AS h,
+
+          (
+              SELECT ps2."Available Trim Quota [RO]"
+              FROM poolstk ps2
+              WHERE TRIM(LOWER(ps2."Variant Description")) = TRIM(LOWER(vna_calc."Trim"))
+              ORDER BY ps2.id
+              LIMIT 1
+          ) AS i
+
+      FROM vna_calc`,[dealer_id,selectedDealerCode]);
+
+  const worksheet = XLSX.utils.json_to_sheet(result.rows);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Inventory");
+
+  // Create buffer instead of file
+  const buffer = XLSX.write(workbook, {
+    type: "buffer",
+    bookType: "xlsx",
+  });
+
+  res.setHeader(
+  "Content-Type",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+);
+res.setHeader(
+  "Content-Disposition",
+  "attachment; filename=dealer_inventory.xlsx"
+);
+
+return res.send(buffer);
+
+
+}
+
+async function DownloadOriginalVna(req, res) {
+
+    const {dealer_id} = req.params;
+    console.log(dealer_id)
+
+  const result = await pool.query("SELECT * FROM vna where dealer_id = $1",[dealer_id]);
+
+  const worksheet = XLSX.utils.json_to_sheet(result.rows);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Inventory");
+
+  // Create buffer instead of file
+  const buffer = XLSX.write(workbook, {
+    type: "buffer",
+    bookType: "xlsx",
+  });
+
+  res.setHeader(
+  "Content-Type",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+);
+res.setHeader(
+  "Content-Disposition",
+  "attachment; filename=dealer_inventory.xlsx"
+);
+
+return res.send(buffer);
+
+
+}
+
+
+async function DownloadPoolstock(req, res) {
+
+
+  const result = await pool.query("SELECT * FROM poolstock_data");
+
+  const worksheet = XLSX.utils.json_to_sheet(result.rows);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Inventory");
+
+  // Create buffer instead of file
+  const buffer = XLSX.write(workbook, {
+    type: "buffer",
+    bookType: "xlsx",
+  });
+
+  res.setHeader(
+  "Content-Type",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+);
+res.setHeader(
+  "Content-Disposition",
+  "attachment; filename=dealer_inventory.xlsx"
+);
+
+return res.send(buffer);
+
+
+}
 
 module.exports = {
     DownloadInventory,
-    DownloadBBNDInventory
+    DownloadBBNDInventory,
+    DownloadMatchedExcel,
+    DownloadOriginalVna,
+    DownloadPoolstock
 }
--- routes/ExcelDownloads.routes.js
+++ routes/ExcelDownloads.routes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const {DownloadInventory, DownloadBBNDInventory} =  require('../controller/ExcelDownload.controller');
+const {DownloadInventory, DownloadBBNDInventory, DownloadMatchedExcel, DownloadOriginalVna, DownloadPoolstock} =  require('../controller/ExcelDownload.controller');
 
 router.get('/DownloadInventory/:dealer_id',DownloadInventory)
 router.get('/DownloadBBNDInventory/:dealer_id',DownloadBBNDInventory)
+router.get('/DownloadMatchedExcel/:dealer_id/:selectedDealerCode',DownloadMatchedExcel)
+router.get('/DownloadOriginalVna/:dealer_id',DownloadOriginalVna)
+router.get('/DownloadPoolstock',DownloadPoolstock)
+
 
 
 
